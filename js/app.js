3d404cc67b681b1922940771cb11b336c8510994
Iryna Korolova <korolova.lviv@gmail.com>
2021-09-14 16:52:57 +0300

    minor improvements added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,24 @@
 const formId = document.getElementById("form");
 const formElements = formId.elements;
-const submitBtn = document.getElementById("submit");
 
 const getFormData = () => {
-  let data = { [formId]: {} }; // create an empty object with the formIdentifier as the key and an empty object as its value
+  let data = {};
   for (const element of formElements) {
-    if (element.name.length > 0) {
-      data[formId][element.name] = element.value;
+    if (element.value.length > 0) {
+      data[element.name] = element.value;
     }
   }
   return data;
 };
 
-submitBtn.onclick = (event) => {
-  event.preventDefault();
-  data = getFormData();
-  localStorage.setItem(formId, JSON.stringify(data[formId]));
-};
+formId.addEventListener("change", (e) => {
+  let data = getFormData();
+  localStorage.setItem("formData", JSON.stringify(data));
+});
 
 const populateForm = () => {
-  if (localStorage.key(formId)) {
-    const savedData = JSON.parse(localStorage.getItem(formId)); // get and parse the saved data from localStorage
+  if (localStorage.key("formData")) {
+    const savedData = JSON.parse(localStorage.getItem("formData")); // get and parse the saved data from localStorage
     for (const element of formElements) {
       if (element.name in savedData) {
         element.value = savedData[element.name];
@@ -28,4 +26,5 @@ const populateForm = () => {
     }
   }
 };
-document.onload = populateForm();
+
+populateForm();
